4aa50dd81db510307bf523c9b66eba020ed32f78
Philippe <p.lee9519@gmail.com>
2022-11-20 17:00:07 -0500

    UCS almost done

--- node.py
+++ node.py
@@ -4,22 +4,32 @@ from board import Board
 class Node:
     GOAL_POSITION = 17  # array position so like starting from 0
 
-    def __init__(self, board: Board):
+    def __init__(self, board: Board, parentNode):
         self.board = board
-        self.parentNode = None
+        self.parentNode = parentNode
 
+    def __eq__(self, other):
+        return self.board.board == other.board
     def setParentNode(self, parentNode):
-        self.parentNode = Node(parentNode)
+        self.parentNode = parentNode
 
     def getParentNode(self):
         return self.parentNode
 
-    def generateChildren(self, closedList: list[Board]) -> list[Board]:
-        children = self.board.allPossibleMoves()
-        for node in closedList:
-            if node in children:
-                children.remove(node)
-        return children
+    def generateChildren(self, closedListOfNodes, openListofNodes):
+        newNodes = []
+        childrenBoards = self.board.allPossibleMoves()
+        #Create new Nodes
+        for oneBoard in childrenBoards:
+            newNodes.append(Node(oneBoard,self))
+        for node in newNodes:
+            if node.getBoard() in closedListOfNodes:
+                newNodes.remove(node)
+            if node.getBoard() in openListofNodes:
+                newNodes.remove(node)
+        return newNodes
 
+    def getBoard(self):
+        return self.board
     def __str__(self) -> str:
         return f"Current node: \n{self.board.boardToString()} \nParent Node: \n{self.parentNode.board.boardToString()}"
--- ucs.py
+++ ucs.py
@@ -1,16 +1,51 @@
 from node import Node
 from collections import deque
 from board import Board
-
 closed = []
 open = deque()
 
 def UCS(initialBoard: Board):
-    if initialBoard.getVehicleAtExit() == "A":
-        pass
-    open.append("slay")
-    open.append("queen")
-    open.popleft()
-    print(open)
+    initialNode = Node(initialBoard,None)
 
-ucs = UCS()
+    open.append(initialNode)
+
+    while(len(open)>0):
+
+        #Taking the next node in the open list
+        theNode = open.popleft()
+
+        #Append it to the closed list
+        closed.append(theNode)
+
+        #Check if it is in the goal state
+        if theNode.getBoard().getVehicleAtExit() == "A":
+            return theNode
+
+        #Append all the children into the end of the open list
+        theChildrenOfThisNode = theNode.generateChildren(closed,open)
+        open.extend(theChildrenOfThisNode)
+
+
+
+
+
+    # theChildrenOfThisNode = open[0].generateChildren(closed)
+    # open.extend(theChildrenOfThisNode)
+    #
+    # for node in theChildrenOfThisNode:
+    #     if node.getBoard() in open:
+    #         open.remove(node)
+    #
+    # printAllBoardsInAListOfNodes(open)
+            # open.append("slay")
+            # open.append("queen")
+            # open.popleft()
+            # print(open)
+
+    return None;
+# ucs = UCS()
+
+def printAllBoardsInAListOfNodes(ListOfNodes):
+    for eachNode in ListOfNodes:
+        print(eachNode.getBoard().boardToString())
+        print()
