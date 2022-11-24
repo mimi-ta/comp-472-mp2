9e0736a62736e16704325e3df67b07520f2f50b3
Mimi Ta <mimi_ta3000@hotmail.com>
2022-11-24 02:15:13 -0500

    Clean up messy code from writing to file and stuff

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
+import copy
+import re
+from io import TextIOWrapper
+
 from board import Board
 from node import Node
 from puzzleParser import PuzzleParser
 from ucs import UCS
-import re
-import copy
 
 INPUT_FILE = "sample-input.txt"
+
+
+def getRuntimeAndPrintStuffToConsole(winningNode, isWin):
+    if isWin:
+        print("You've Won!")
+        runtime = winningNode.runtime
+        print(f"Winning board:\n{winningNode.getBoard().boardToString()}")
+    else:
+        runtime = winningNode
+        print(f"Runtime: {runtime} seconds")
+        print("No solution.\n\n")
+
+
+def writeSolutionPath(winningNode, f: TextIOWrapper):
+    f.write(f"Solution path: ")
+    winningNodeIterator = copy.deepcopy(winningNode)
+    while winningNodeIterator.parentNode != None:
+        f.write(
+            re.split(r"\t+", winningNodeIterator.board.move)[0] + "; "
+            if winningNodeIterator.parentNode != None
+            else ""
+        )
+        winningNodeIterator = winningNodeIterator.parentNode
+
+    f.write("\n\n")
+
+    winningNodeIterator = winningNode
+    while winningNodeIterator.parentNode != None:
+        f.write(winningNodeIterator.board.move + "\n")
+        winningNodeIterator = winningNodeIterator.parentNode
 
 
 def generateUcsOutputFiles(i, puzzle: list[str]):
@@ -16,20 +48,11 @@ def generateUcsOutputFiles(i, puzzle: list[str]):
     f.write(board.boardToString() + "\n")
     print(f"Initial board:\n{board.boardToString()}")
     f.write(f"Car fuel available: {board.getAllCarFuels()}\n\n")
+    runtime = 0
 
     winningNode = UCS(board)
-    
-    winningNodeIterator = copy.deepcopy(winningNode)
-    runtime = 0
     isWin = type(winningNode) == type(Node(None, None))
-    if isWin:
-        print("You've Won!")
-        runtime = winningNode.runtime
-        print(f"Winning board:\n{winningNode.getBoard().boardToString()}")
-    else:
-        runtime = winningNode
-        print(f"Runtime: {runtime} seconds")
-        print("No solution.\n\n")
+    runtime = getRuntimeAndPrintStuffToConsole(winningNode, isWin)
 
     f.write(f"Runtime: {runtime} seconds\n")
 
@@ -37,22 +60,13 @@ def generateUcsOutputFiles(i, puzzle: list[str]):
         f.write(f"Search path length: states\n")  # TODO
         f.write(f"Solution path length: moves\n")  # TODO
 
-        f.write(f"Solution path: ")
-        while winningNodeIterator.parentNode != None:
-            f.write(re.split(r'\t+', winningNodeIterator.board.move)[0] + "; " if winningNodeIterator.parentNode != None else "")
-            winningNodeIterator = winningNodeIterator.parentNode
-
-        f.write("\n\n")
-
-        winningNodeIterator = winningNode
-        while winningNode.parentNode != None:
-            f.write(winningNode.board.move + "\n")
-            winningNode = winningNode.parentNode
+        writeSolutionPath(winningNode, f)
 
         f.write("\n")
         f.write(winningNode.getBoard().boardToString())
     else:
         f.write("No solution.")
+
 
 def main():
     f = open(INPUT_FILE, "r")
